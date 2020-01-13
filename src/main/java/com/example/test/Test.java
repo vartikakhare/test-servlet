ee0f73c68a104a5dab84481d12c7d94fa1e6048c
vartikakhare <58975582+vartikakhare@users.noreply.github.com>
2020-01-13 16:24:36 +0530

    Update Test.java

--- src/main/java/com/example/test/Test.java
+++ src/main/java/com/example/test/Test.java
@@ -24,7 +24,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Welcome to the servlet");
+		response.getWriter().append("Heloo! Welcome to the Test Servlet! ");
 	}
 
 	/**
